ea995b0c2b999fa0503acc043293f93215850083
Nannan <nanna7077@gmail.com>
2021-11-28 10:16:58 +0530

    Add files via upload

---
+++ wallux-desktop.py
@@ -0,0 +1,131 @@
+import eel
+import os
+import sys
+import requests
+
+baseurl = "https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/"
+
+eel.init('web')
+
+@eel.expose
+def download_wallpaper(wallfile):
+    if os.name=='nt':
+        req = requests.get(baseurl+wallfile, stream=True)
+        if req:
+            img = req.raw.read()
+            path = os.path.expanduser("~")
+            path=path+"\\Pictures\\Wallux"
+            try:
+                os.mkdir(path)
+            except:
+                pass
+            path=path+'\\'+(wallfile.lstrip("wallpapers/"))
+            with open(path, 'wb') as f:
+                f.write(img)
+            return "Wallpaper Saved to {}".format(path)
+        else:
+            return "Could not download wallpaper ;-;"
+    else:
+        req = requests.get(baseurl+wallfile, stream=True)
+        if req:
+            img = req.raw.read()
+            path = os.path.expanduser("~")
+            path=path+"/Pictures/Wallux"
+            try:
+                os.mkdir(path)
+            except:
+                pass
+            path=path+'/'+(wallfile.lstrip("wallpapers/"))
+            with open(path, 'wb') as f:
+                f.write(img)
+            return "Wallpaper Saved to {}".format(path)
+        else:
+            return "Could not download wallpaper ;-;"
+
+@eel.expose
+def set_wallpaper(wallfile):
+    if os.name=="nt":
+        import ctypes
+        req = requests.get(baseurl+wallfile, stream=True)
+        if req:
+            img = req.raw.read()
+            path = os.path.expanduser("~")
+            path=path+"\\Pictures\\Wallux"
+            try:
+                os.mkdir(path)
+            except:
+                pass
+            path=path+'\\'+(wallfile.lstrip("wallpapers/"))
+            with open(path, 'wb') as f:
+                f.write(img)
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+        else:
+            return "Could not get wallpaper. Try again!"
+    else:
+        req = requests.get(baseurl+wallfile, stream=True)
+        if req:
+            img = req.raw.read()
+            path = os.path.expanduser("~")
+            path=path+"/Pictures/Wallux"
+            try:
+                os.mkdir(path)
+            except:
+                pass
+            path=path+'/'+(wallfile.lstrip("wallpapers/"))
+            with open(path, 'wb') as f:
+                f.write(img)
+        else:
+            return "Could not get wallpaper. Try again!"
+        os.system("""echo $(ps -e | grep -E -i "xfce|kde|gnome") > /tmp/wallux.file""")
+        parseStr = ''
+        with open("/tmp/wallux.file") as f:
+            parseStr = f.read()
+        os.remove("/tmp/wallux.file")
+        de = {}
+        de['kde'] = parseStr.lower().count("kde")
+        de['gnome'] = parseStr.lower().count('gnome')
+        de['xfce'] = parseStr.lower().count('xfce')
+        if max(de, key=de.get) == "gnome":
+            os.system(
+                "gsettings set org.gnome.desktop.background picture-uri file://{}".format(path))
+            return "Wallpaper Set Successfully!"
+        elif max(de, key=de.get) == "kde":
+            import dbus
+            plugin = 'org.kde.image'
+            jscript = """
+            var allDesktops = desktops();
+            print (allDesktops);
+            for (i=0;i<allDesktops.length;i++) {
+                d = allDesktops[i];
+                d.wallpaperPlugin = "%s";
+                d.currentConfigGroup = Array("Wallpaper", "%s", "General");
+                d.writeConfig("Image", "file://%s")
+            }
+            """
+            bus = dbus.SessionBus()
+            plasma = dbus.Interface(bus.get_object(
+                'org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
+            plasma.evaluateScript(jscript % (plugin, plugin, path))
+            return "Wallpaper Set Successfully!"
+        else:
+            return "Desktop Environment not yet supported. Please create an issue at Wallux-Desktop repository to get support!"
+    return "Wallpaper Set Successfully"
+
+@eel.expose
+def closed():
+    sys.exit()
+
+if os.name=="nt":
+    x=os.system("start firefox -kiosk -private-window http://127.0.0.1:60001")
+    if x==1:
+        x=os.system("start chrome -kiosk -private-window http://127.0.0.1:60001")
+    if x==1:
+        x=os.system("start msedge -kiosk -private-window http://127.0.0.1:60001")
+    eel.start('index.html', mode=None, port=60001)
+else:
+    x=os.system("google-chrome-stable -kiosk -private-window http://127.0.0.1:60001 &")
+    if x==1:
+        x=os.system("firefox -kiosk -private-window http://127.0.0.1:60001 &")
+    if x==1:
+        x=os.system("msedge -kiosk -private-window http://127.0.0.1:60001 &")
+    eel.start('index.html', mode=None, port=60001)
---
+++ web/static/home.js
@@ -0,0 +1,128 @@
+var theWallpaperDict = {};
+
+var ScrollCounter = 0;
+
+function onScrollFunction() {
+    elem = document.getElementById('fullimg');
+    elem.style.top = window.scrollY + 120;
+    elem = document.getElementById('settingwallpaper');
+    elem.style.top = window.scrollY + 140;
+    elem = document.getElementById('downloadingwallpaper');
+    elem.style.top = window.scrollY + 140;
+}
+
+window.addEventListener('scroll', function(event) { onScrollFunction() });
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+
+    return array;
+}
+
+function openfullimage(id) {
+    tagstr = '';
+    id = parseInt(id);
+    for (i in theWallpaperDict) {
+        if (theWallpaperDict[i]['path'].replace(/\D/g, "") == id) {
+            elem = theWallpaperDict[i];
+        }
+    }
+    arr = elem['tags'];
+    for (j in arr) {
+        tagstr += arr[j];
+        tagstr += ", ";
+    }
+    tagstr = tagstr.slice(0, -2);
+    document.getElementById("fulldisp_img").src = "https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/" + elem['path'];
+    document.getElementById("fullimagedisplay_name").innerHTML = elem['name'];
+    document.getElementById("fullimagedisplay_desc").innerHTML = elem['description'];
+    document.getElementById("fullimagedisplay_id").innerHTML = "Wallux ID: " + elem['path'].replace(/\D/g, "");
+    document.getElementById("fullimagedisplay_tags").innerHTML = tagstr;
+    document.getElementById("fullimagedisplay_link_download_primary").onclick = function(){document.getElementById('downloadingwallpaper').style.display='block'; eel.download_wallpaper(elem['path'])(function(message){document.getElementById('downloadingwallpaper').style.display='none'; window.alert(message);});}
+    document.getElementById("fullimagedisplay_link_set_as_wallpaper").onclick = function(){document.getElementById('settingwallpaper').style.display='block'; eel.set_wallpaper(elem['path'])(function(message){document.getElementById('settingwallpaper').style.display='none'; window.alert(message);});}
+    document.getElementById("fullimagedisplay_link_set_as_wallpaper_secondary").onclick = function(){document.getElementById('settingwallpaper').style.display='block'; eel.set_wallpaper(elem['path'])(function(message){document.getElementById('settingwallpaper').style.display='none'; window.alert(message);});}
+    document.getElementById("fullimagedisplay_link_download_secondary").onclick = function(){document.getElementById('downloadingwallpaper').style.display='block'; eel.download_wallpaper(elem['path'])(function(message){document.getElementById('downloadingwallpaper').style.display='none'; window.alert(message);});}
+    document.getElementById("fullimg").style.display = 'block';
+}
+
+function onlyTags(tag) {
+    final_array = shuffle(theWallpaperDict);
+    var container_ = document.getElementById("randomContainer");
+    container_.innerHTML = '';
+    for (i in final_array) {
+        tagstr = '';
+        arr = final_array[i]['tags'];
+        if (arr.indexOf(tag) >= 0) {
+            for (j in arr) {
+                tagstr += arr[j];
+                tagstr += ", ";
+            }
+            tagstr = tagstr.slice(0, -2);
+            container_.innerHTML +=
+                "<div class=\"glassyContainer\" onclick=openfullimage(" + final_array[i]['path'].replace(/\D/g, "") + ")> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "</b></div></div>"; //<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") + 
+        }
+    }
+}
+
+function workwithdata(data) {
+    var final_array = [];
+    final_array = shuffle(data['wallpaper']);
+    var container_ = document.getElementById("randomContainer");
+    container_.innerHTML = '';
+    for (i in final_array) {
+        tagstr = '';
+        arr = final_array[i]['tags'];
+        for (j in arr) {
+            tagstr += arr[j];
+            tagstr += ", ";
+        }
+        tagstr = tagstr.slice(0, -2);
+        container_.innerHTML +=
+            "<div class=\"glassyContainer\" onclick=openfullimage(" + final_array[i]['path'].replace(/\D/g, "") + ")> <img class = \"imgDisp\" loading=\"lazy\" src = \"https://raw.githubusercontent.com/Wallux-0/Wallpapers/main/compressed/" + final_array[i]['path'] + "\"><div class=\"imgText\"> <b>" + final_array[i]['name'] + "</b><br>Description: " + final_array[i]['description'] + "<br>" + tagstr + "</b></div></div>"; //"<br>Wallux ID: <b>" + final_array[i]['path'].replace(/\D/g, "") +
+    }
+    var tagContainer = document.getElementById("tagContainer");
+    tagContainer.innerHTML = '';
+    tags = '';
+    tarr = [];
+    for (i in final_array) {
+        arr = final_array[i]['tags'];
+        for (j in arr) {
+            if (tarr.indexOf(arr[j]) < 0) {
+                tarr.push(arr[j]);
+                tags += "<a onclick=\"onlyTags(\'" + arr[j] + "\')\"> <div class=\"tag\">" + arr[j] + "</div> </a>";
+            }
+        }
+    }
+    tagContainer.innerHTML = tags;
+    theWallpaperDict = final_array;
+};
+
+$.ajax({
+    url: "https://raw.githubusercontent.com/Wallux-0/Wallux/main/static/tags.json",
+    success: function(data) {
+        workwithdata(data);
+    },
+    error: function(data) {
+        document.getElementById("noInternet").style.display = "block";
+    },
+    dataType: "json",
+    timeout: 3000
+});
+
+window.addEventListener('mouseup', function(event) {
+    var pol = document.getElementById('fullimg');
+    if (event.target != pol && event.target.parentNode != pol) {
+        document.getElementById("fulldisp_img").src = "";
+        pol.style.display = 'none';
+    }
+});
+
+$(window).bind('beforeunload', function() {eel.closed()})
